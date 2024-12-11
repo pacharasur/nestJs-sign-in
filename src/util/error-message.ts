fe96033c89ff4e6214dbbbb6fc6fe96ef49c3be3
pacharasur <94338617+pacharasur@users.noreply.github.com>
2024-12-11 15:56:00 +0700

    add enum exist user

--- src/util/error-message.ts
+++ src/util/error-message.ts
@@ -2,6 +2,7 @@ export enum ErrorMessage {
   ACCESS_DENIED = 'ปฏิเสธการเข้าถึง',
   UNAUTHORIZED = 'ไม่ได้รับอนุญาตในการใช้งาน',
   USER_NOT_FOUND = 'ไม่พบผู้ใช้นี้',
+  USER_EXISTS = 'มีชื่อผู้ใช้นี้ในระบบแล้ว',
   CONTACT_SERIVCE = 'เกิดข้อผิดพลาด กรุณาติดต่อเจ้าหน้าที่',
   INCORRECT_PASSWORD = 'รหัสผ่านไม่ถูกต้อง',
   SAVE_FAILED = 'บันทึกข้อมูลไม่สำเร็จ',
